3c6ebd972583b92324877ecd98ddce26347e7f93
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-28 16:18:10 +0530

    Delete input_schema.py

--- input_schema.py
+++
@@ -1,8 +0,0 @@
-INPUT_SCHEMA = {
-    "url": {
-        'datatype': 'STRING',
-        'required': True,
-        'shape': [1],
-        'example': ["https://github.com/rbgo404/Files/raw/main/scene0000_00.ply"]
-    }
-}
